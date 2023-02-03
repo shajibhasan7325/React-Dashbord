39d744d17a9b21066e84b3e911aea9a320779b34
unknown <shajibhasan7325@gmail.com>
2023-02-03 09:03:23 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import Dashbord from "./scenes/dashboard";
+
+import Team from "./scenes/team/team";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import FAQ from "./scenes/faq";
+// import Calender from "./scenes/calender";
+import Geography from "./scenes/geography";
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashbord />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/geography" element={<Geography />} />
+              {/* <Route path="/calender" element={<Calender />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
